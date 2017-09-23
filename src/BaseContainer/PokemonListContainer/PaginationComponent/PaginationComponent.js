cda3fdb3d1f991b1f8d7ba46138cd35c51e9833a
Jaime <jaime.mateoh@gmail.com>
2017-09-23 21:08:02 +0200

    Disable button if it is current page

--- src/BaseContainer/PokemonListContainer/PaginationComponent/PaginationComponent.js
+++ src/BaseContainer/PokemonListContainer/PaginationComponent/PaginationComponent.js
@@ -6,7 +6,7 @@ class PaginationComponent extends Component {
 		super(props);
 
 		this.state = {
-      currentPage: 1,
+      currentPage: 0,
     };
 		
 		this.GoToPage = this.GoToPage.bind(this)
@@ -14,25 +14,26 @@ class PaginationComponent extends Component {
 
 	GoToPage(page) {
 		this.props.callbackFromParent(page)
+		this.setState({currentPage: page})
 	}
 
 	render() {
 		return (
 			<ul>Pagination Component
 				<li>
-					<button type="button" onClick={() => { this.GoToPage(1) }}> 1 </button>
+					<button type="button" onClick={() => { this.GoToPage(0) }} disabled = {this.state.currentPage === 0} > 1 </button>
 				</li>
 				<li>
-					<button type="button" onClick={() => { this.GoToPage(2) }}> 2 </button>
+					<button type="button" onClick={() => { this.GoToPage(1) }} disabled = {this.state.currentPage === 1} > 2 </button>
 				</li>
 				<li>
-					<button type="button" onClick={() => { this.GoToPage(3) }}> 3 </button>
+					<button type="button" onClick={() => { this.GoToPage(2) }} disabled = {this.state.currentPage === 2} > 3 </button>
 				</li>
 				<li>
-					<button type="button" onClick={() => { this.GoToPage(4) }}> 4 </button>
+					<button type="button" onClick={() => { this.GoToPage(3) }} disabled = {this.state.currentPage === 3} > 4 </button>
 				</li>
 				<li>
-					<button type="button" onClick={() => { this.GoToPage(5) }}> 5 </button>
+					<button type="button" onClick={() => { this.GoToPage(4) }} disabled = {this.state.currentPage === 4} > 5 </button>
 				</li>
 			</ul>
 		);
