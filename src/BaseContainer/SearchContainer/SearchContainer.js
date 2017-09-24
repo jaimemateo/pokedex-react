9ffc42e31293a81b37931a69c9a77e140b416926
Jaime <jaime.mateoh@gmail.com>
2017-09-24 16:29:17 +0200

    Yarn eject to install Scss and webpack loader config

--- src/BaseContainer/SearchContainer/SearchContainer.js
+++ src/BaseContainer/SearchContainer/SearchContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import PokemonListContainer from '../PokemonListContainer';
-import './styles.css';
+import './styles.scss';
 
 class SearchContainer extends Component {
 
